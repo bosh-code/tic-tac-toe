0bff5146a9645e3d085a20d2903887f84203875f
bosh-code <ryan.julian.bosher@icloud.com>
2021-06-04 14:54:05 +1200

    add check for win function

--- utils.js
+++ utils.js
@@ -1,8 +1,16 @@
-let testWin = ['x', false, false, 'x', false, false, 'x', false, false]
-
-let indexes = []
+const winConditionsArr = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
 function getWinArr (arr, value) {
+  let indexes = []
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === value) {
       indexes.push(i)
@@ -10,40 +18,15 @@ function getWinArr (arr, value) {
   }
   return indexes
 }
-getWinArr(testWin, 'x')
 
-
-function checkWin(arr) {
-
-  // console.log(arr)
-
-  const winConditionsArr = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ]
-  console.log('index', indexes)
-  console.log('win condit', winConditionsArr[3])
-  if (arr[1] === winConditionsArr[3][1]) {
-    console.log('win')
-  } else {
-    console.log('err')
-  }
-  
-  
-  // for (let i = 0; i < 8; i++) {
-  //   // console.log('loop log', winConditionsArr[i])
-  //   // check if indexes is equal to any win condition
-  //   if (arr === winConditionsArr[3]) {
-  //     console.log('win')
-  //   }
-    
-  // }
+function checkWin (arr) {
+  let result = false
+  winConditionsArr.forEach(function (innerArr) {
+    if (innerArr.every((val) => arr.includes(val)) === true) {
+      result = true
+    }
+  })
+  return result
 }
 
-checkWin(indexes)
+module.exports = {getWinArr, checkWin}
